9023b8b7279e899fb42a6b2e3b7258e09b9a7b84
Brecht Savelkoul <brecht.savelkoul@alumni.lse.ac.uk>
2020-05-29 20:49:14 +0200

    Switch to ProseMirror

--- Makefile
+++
@@ -1,9 +0,0 @@
-.PHONY: all deploy
-
-all: public/ckeditor.js
-
-deploy: all
-	surge ./public pamphlets-editor.surge.sh
-
-public/ckeditor.js: src/ckeditor.js
-	node_modules/.bin/webpack --mode production
---
+++ index.js
@@ -0,0 +1,55 @@
+var { EditorState } = require('prosemirror-state')
+var { EditorView } = require('prosemirror-view')
+var { baseKeymap } = require('prosemirror-commands')
+var { buildInputRules, buildKeymap } = require('prosemirror-example-setup')
+var { buildMarkdownRules } = require('./lib/markdown-rules')
+var { dropCursor } = require('prosemirror-dropcursor')
+var { history } = require('prosemirror-history')
+var { keymap } = require('prosemirror-keymap')
+var { schema, defaultMarkdownParser, defaultMarkdownSerializer } = require('prosemirror-markdown')
+var { toggleLink } = require('./lib/toggle-link')
+
+class DraftEditor {
+  constructor (container, content = '') {
+    this.container = container
+    this.view = this.createView(content)
+  }
+
+  createState (content) {
+    return EditorState.create({
+      schema,
+      doc: defaultMarkdownParser.parse(content),
+      plugins: [
+        buildInputRules(schema),
+        buildMarkdownRules(schema),
+        history(),
+        keymap(buildKeymap(schema)),
+        keymap({ 'Mod-h': toggleLink, 'Mod-H': toggleLink }),
+        keymap(baseKeymap),
+        dropCursor()
+      ]
+    })
+  }
+
+  createView (content) {
+    var state = this.createState(content)
+    var view = new EditorView(this.container, { state })
+    view.dom.style.preWrap = 'pre-wrap'
+    return view
+  }
+
+  get content () {
+    return defaultMarkdownSerializer.serialize(this.view.state.doc)
+  }
+
+  set content (content) {
+    this.view.destroy()
+    this.view = this.createView(content)
+  }
+
+  get state () {
+    return this.view.state
+  }
+}
+
+module.exports = DraftEditor
